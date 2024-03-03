fa855813c86f0db5225014f2f4adcaaa3ee435f0
JuanMiranda1998 <mirandajuan1998@outlook.com>
2024-03-02 21:58:44 -0300

    styled modal

--- src/app/components/MainSection/DonationModal.jsx
+++ src/app/components/MainSection/DonationModal.jsx
@@ -13,9 +13,9 @@ const DonationModal = ({ toggleModalFunction }) => {
    }
 
   return (
-    <div className='absolute -top-16 left-1/2 -translate-y-10 -translate-x-1/2 flex flex-col py-10 px-10 shadow-md rounded-lg z-20 bg-white'>
+    <div className='absolute -top-16 left-1/2 -translate-y-10 -translate-x-1/2 flex flex-col py-8 px-10 shadow-md rounded-lg z-20 bg-white'>
         {donationCompleted ? <ThankYouModal toggleDonationFunction={toggleDonation} /> : 
-        (<div className='w-80 md:w-[760px]'>
+        (<div className='w-80 md:w-[720px]'>
           <div className='absolute right-6 top-8 flex items-center justify-center w-8 h-8 cursor-pointer' onClick={toggleModalFunction}>
             <Image
               src={CloseModalIcon}
@@ -27,9 +27,14 @@ const DonationModal = ({ toggleModalFunction }) => {
           <h1 className='text-xl md:text-2xl font-semibold my-4'>Back this project</h1>
           <p className='text-base text-darkGray font-medium mb-4'>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
           <div className='flex flex-col gap-6 my-6'>
-            <div className='flex flex-col justify-between items-center px-8 py-10 rounded-lg border border-[#dddddd]'>
-                <h2 className='text-xl font-semibold'>Pledge with no reward</h2>
-                <p className='text-darkGray font-medium text-base my-8'>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.</p>
+            <div className='grid grid-cols-12 grid-rows-2 items-center place-items-center px-8 py-10 rounded-lg border border-[#dddddd]'>
+                <div className='col-span-1 row-span-1 mr-8'>
+                  <input className='w-6 h-6' type="radio" name="noReward" id="noReward" />
+                </div>
+                <div className='col-span-11 row-span-2'>
+                  <h2 className='text-xl font-semibold my-2'>Pledge with no reward</h2>
+                  <p className='text-darkGray font-medium text-base my-4'>Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.</p>
+                </div>
             </div>
             {
               OPTIONS_DATA.map((option) => (
--- src/app/components/MainSection/OptionModalCard.jsx
+++ src/app/components/MainSection/OptionModalCard.jsx
@@ -13,15 +13,19 @@ const OptionModalCard = ({
 
     
     return (
-        <div className={`flex flex-col px-8 py-10 rounded-lg border border-[#dddddd] ${isAvailable ? '' : 'opacity-50'}`}>
-            <div className='flex flex-row justify-between items-center'>
-                <h2 className='text-xl font-semibold'>{title}</h2>
-                <p className='text-base font-medium text-moderateCyan'>Pledge ${minimumTake} or more</p>
+        <div className={`grid grid-cols-12 grid-rows-2 px-8 py-10 rounded-lg border border-[#dddddd] ${isAvailable ? '' : 'opacity-50'}`}>
+            <div className='col-span-1 row-span-2 mr-8'>
+                  <input className='w-6 h-6' type="radio" />
             </div>
-            <p className='text-darkGray font-medium text-base my-8'>{description}</p>
-            <div className='flex flex-row justify-between items-center'>
+            <div className='flex flex-row justify-between col-span-11 row-span-1'>
+                <div className='flex flex-row gap-4'>
+                    <h2 className='text-xl font-semibold'>{title}</h2>
+                    <p className='text-base font-medium text-moderateCyan mt-1'>Pledge ${minimumTake} or more</p>
+                </div>
                 <p className='text-darkGray font-medium'><span className='text-3xl font-bold text-black mr-2'>100</span>left</p>
-                <button className={`text-white text-sm font-medium ${isAvailable ? 'bg-moderateCyan hover:bg-darkCyan transition-colors ease-in duration-200' : 'bg-darkGray'} rounded-full px-8 py-3`} disabled={!isAvailable} onClick={toggleDonationFunction} >{isAvailable ? 'Select Reward' : 'Out of stock'}</button>
+            </div>
+            <div className='col-span-11 row-span-1'>
+                <p className='text-darkGray font-medium text-base'>{description}</p>
             </div>
         </div>
     )
